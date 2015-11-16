444540a7d215d9605972e2b52e2fa0d585367ad9
romanornr <romanornr@gmail.com>
2015-11-16 22:46:28 +0100

    Array and CSS

--- week1/opdr3.php
+++ week1/opdr3.php
@@ -16,7 +16,76 @@ $albums = array(
 				'year_release'	=> 2015,
 				'tracks' 		=> 5,
 				'genre' 		=> 'pop'
+				), 
+
+			array(
+				'id'			=> 3,
+				'album_name' 	=> 'Molly',
+				'artist' 		=> 'Tyga',
+				'year_release'	=> 2013,
+				'tracks' 		=> 6,
+				'genre' 		=> 'hip hop'
+				),
+			array(
+				'id'			=> 4,
+				'album_name' 	=> 'We Love Disney',
+				'artist' 		=> 'Various Artist',
+				'year_release'	=> 2015,
+				'tracks' 		=> 12,
+				'genre' 		=> 'pop'
+				),
+			array(
+				'id'			=> 5,
+				'album_name' 	=> '1989',
+				'artist' 		=> 'Taylor Swift',
+				'year_release'	=> 2015,
+				'tracks' 		=> 13,
+				'genre' 		=> 'pop'
+				),
+			array(
+				'id'			=> 6,
+				'album_name' 	=> 'Revival',
+				'artist' 		=> 'Selena Gomez',
+				'year_release'	=> 2015,
+				'tracks' 		=> 8,
+				'genre' 		=> 'pop'
+				),
+			array(
+				'id'			=> 7,
+				'album_name' 	=> '21',
+				'artist' 		=> 'Adele',
+				'year_release'	=> 2015,
+				'tracks' 		=> 8,
+				'genre' 		=> 'pop'
+				),
+			array(
+				'id'			=> 8,
+				'album_name' 	=> 'Confident',
+				'artist' 		=> 'Demi Lovato',
+				'year_release'	=> 2015,
+				'tracks' 		=> 14,
+				'genre' 		=> 'pop'
+				),
+			array(
+				'id'			=> 9,
+				'album_name' 	=> 'Buy Me A Boat',
+				'artist' 		=> 'Adele',
+				'year_release'	=> 2015,
+				'tracks' 		=> 8,
+				'genre' 		=> 'pop'
+				),
+			array(
+				'id'			=> 10,
+				'album_name' 	=> 'Nothing was the Same',
+				'artist' 		=> 'Drake',
+				'year_release'	=> 2015,
+				'tracks' 		=> 5,
+				'genre' 		=> 'pop'
 				)
+
+
+
+
 
 		);
 ?>
@@ -24,8 +93,10 @@ $albums = array(
 <html>
 <head>
 <style> 
-tr:nth-child(odd) {background: #FFF}
-tr:nth-child(even) {background: #CCC}
+tr:nth-child(odd) {background: #CCC}
+tr:nth-child(odd) {color: white}
+tr:nth-child(even) {background: #FFF}
+
 </style>
 </head>
 
